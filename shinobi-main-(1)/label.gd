a2d443a2e035ea3c3aab84de9edb8acbafd50d41
SDavidson3404 <bd1127702@student.francistuttle.edu>
2025-11-21 12:47:42 -0600

    uhhh

--- shinobi-main-(1)/label.gd
+++
@@ -1,9 +0,0 @@
-extends Label
-
-# ====================
-# PHYSICS PROCESS
-# ====================
-# Runs 60 frames a second
-func _physics_process(_delta: float) -> void:
-	# Set text to amount of points
-	text = "Player Points: " + str(SkillManager.player_points)
